f8176c03218349835771242ad4ed3a0f5f08fa3a
Ohid <ohid1986@users.noreply.github.com>
2016-04-28 09:20:12 +0600

    First commit
    Commit after Template completion

---
+++ organizer/views.py
@@ -0,0 +1,13 @@
+from django.http.response import HttpResponse
+from django.template import Context, loader
+
+from .models import Tag
+
+
+def homepage(request):
+    tag_list = Tag.objects.all()
+    template = loader.get_template(
+        'organizer/tag_list.html')
+    context = Context({'tag_list': tag_list})
+    output = template.render(context)
+    return HttpResponse(output)
